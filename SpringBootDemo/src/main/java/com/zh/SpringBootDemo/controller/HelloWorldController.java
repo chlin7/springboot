9f71a9548c946d26ea3c67d099800d6f218271af
chlin7 <chlin7@126.com>
2017-07-24 08:51:15 +0800

    统一处理异常
    统一处理异常

--- SpringBootDemo/src/main/java/com/zh/SpringBootDemo/controller/HelloWorldController.java
+++ SpringBootDemo/src/main/java/com/zh/SpringBootDemo/controller/HelloWorldController.java
@@ -3,23 +3,36 @@ package com.zh.SpringBootDemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController  //返回的是json数据
-//@Controller  //跳转到对应的页面
+import com.zh.SpringBootDemo.exception.MyException;
+
+
+//@RestController  //返回的是json数据
+@Controller  //跳转到对应的页面
 @RequestMapping(value="/test") 
 public class HelloWorldController {
 	
 	@RequestMapping("/hello")
-	public String index() {
-		return "Hello World你好";
-	}
+    public String hello() throws Exception {
+        throw new Exception("发生错误");
+    }
+
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
+    }
 	
-//	@RequestMapping("/")
-//    public String index(ModelMap map) {
-//        // 加入一个属性，用来在模板中读取
-//        map.addAttribute("host", "http://blog.didispace.com");
-//        // return模板文件的名称，对应src/main/resources/templates/index.html
-//        return "index";  
-//    }
+//	@RequestMapping("/hello")
+//	public String index() {
+//		return "Hello World你好";
+//	}
+	
+	
+	@RequestMapping("/")
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://blog.didispace.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "index";  
+    }
 }
